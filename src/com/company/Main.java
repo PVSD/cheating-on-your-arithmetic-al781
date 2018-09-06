9b1b834c6979fe78990af1f261609579f8e69138
Alexander <al781@pvsd.org>
2018-09-05 21:09:55 -0400

    Hey Look... This works now!!!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
-public class Main {
+public class Main
+    {
 
-    public static void main(String[] args) {
-	// write your code here
+        public static void main(String[] args)
+        {
+            int solutionOne = 79 + 3 * (4 + 82 - 68) - 7 + 19;
+            int solutionTwo = (179 + 21 + 10) / 7 + 181;
+            int solutionThree = 10389 * 56 * 11 + 2246;
+            System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 = " + solutionOne);
+            System.out.println("(179 + 21 + 10) / 7 + 181 = " + solutionTwo);
+            System.out.println("10389 * 56 * 11 + 2246 = " + solutionThree);
+            //Below is me being cheeky... Pay no attention to that...
+                //System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 = 145");
+                //System.out.println("(179 + 21 + 10) / 7 + 181 = 211");
+                //System.out.println("10389 * 56 * 11 + 2246 = 6401870");
+        }
     }
-}
